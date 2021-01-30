719fea00ff4445c21a349fa88bdd22d7d659075a
Glenn Sabales <glenn.sabales@novare.com.hk>
2021-01-30 17:40:58 +0800

    Added additional assets; Deleted side-menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideMenuComponent } from './side-menu/side-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SideMenuComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
