b51d7ab47563b73cbef6ab6e9b5443f7a1c7659e
Glenn Sabales <glenn.sabales@novare.com.hk>
2021-01-31 14:38:46 +0800

    Added navigation button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Typed from 'typed.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+
+  ngOnInit(): void {
+    const typed = new Typed('#typed', {
+      strings: ['a Developer', 'a Learner', 'an Anime Lover'],
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+      loop: true
+    });
+  }
 }
