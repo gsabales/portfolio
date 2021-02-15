9ded11e5e02db6344e57aea5f6cba4ef9d1bb194
Glenn Sabales <glenn.sabales@novare.com.hk>
2021-02-15 08:49:22 +0800

    Added environment variables for elasticmail config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  host: 'smtp25.elasticemail.com',
+  username: 'glenn.sabales@gmail.com',
+  elastic_mail_password: 'D698E436174934E1A896493CA7CBC3FEF632'
 };
