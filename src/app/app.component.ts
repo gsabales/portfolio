534ec27fd175ccbf710d89df20e6b5a202c1b441
gsabales <glenn.sabales@gmail.com>
2022-03-19 19:30:24 +0800

    Code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,17 +37,6 @@ export class AppComponent implements OnInit, AfterViewInit{
   servicesOffset: number = null;
   contactOffset: number = null;
 
-  emailStatus: boolean;
-  emailStatusMessage: string = null;
-  isLoading = false;
-  emailFormGroup = this.fb.group({
-      name: [null, [Validators.required, Validators.minLength(4)]],
-      email: [null, [Validators.required, Validators.email]],
-      subject: [null, [Validators.required, Validators.minLength(8)]],
-      message: [null, Validators.required]
-    }
-  );
-
   constructor(private generalService: GeneralService,
               private modalService: NgbModal,
               private fb: FormBuilder,
@@ -154,42 +143,6 @@ export class AppComponent implements OnInit, AfterViewInit{
   isWithinRange(value: number, minValue: number, maxValue: number): boolean {
     return value >= minValue && value < maxValue;
   }
-
-  // validateFormControl(fcName: string): boolean {
-  //   return this.emailFormGroup.get(fcName).invalid && (this.emailFormGroup.get(fcName).dirty || this.emailFormGroup.get(fcName).touched);
-  // }
-  //
-  // sendEmail(): void {
-  //   this.isLoading = true;
-  //   // const email = new Email(
-  //   //   this.emailFormGroup.get('name').value,
-  //   //   this.emailFormGroup.get('email').value,
-  //   //   this.emailFormGroup.get('subject').value,
-  //   //   this.emailFormGroup.get('message').value,
-  //   // );
-  //
-  //   // Via SmtpJS
-  //   Email.send({
-  //     Host: environment.host,
-  //     Username: environment.username,
-  //     Password: environment.elastic_mail_password,
-  //     To: environment.username,
-  //     From: environment.username,
-  //     Subject: this.emailFormGroup.get('subject').value,
-  //     Body: `
-  //           <b>Name: </b>${this.emailFormGroup.get('name').value} <br/>
-  //           <b>Email: </b>${this.emailFormGroup.get('email').value}<br />
-  //           <b>Subject: </b>${this.emailFormGroup.get('subject').value}<br />
-  //           <b>Message:</b> <br /> ${this.emailFormGroup.get('message').value} <br><br>
-  //           <i>This is sent as a feedback from my portfolio website</i><br/><br/>
-  //           <b>~End of Message.~</b>`
-  //   }).then(() => {
-  //     this.isLoading = false;
-  //     this.emailFormGroup.reset();
-  //     this.emailStatusMessage = 'Your email has been sent. Thank you for your feedback!';
-  //     setTimeout(() => this.emailStatusMessage = null, 2500);
-  //   });
-  // }
 
   // For debugging purposes
   logYOffsets(): void {
